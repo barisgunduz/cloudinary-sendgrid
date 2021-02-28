dcc33ade2d03e6b4918def139baa35ff5029f713
Barış Gündüz <baris.gunduz@devstup.org>
2021-02-28 20:25:39 +0300

    server and client

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+// import route
+
+const feedbackRoutes = require('./routes/feedback')
+
+// app
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cors());
+
+// routes
+
+app.use('/api', feedbackRoutes)
+
+// port
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
