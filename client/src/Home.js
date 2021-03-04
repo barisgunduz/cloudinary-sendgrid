e7ca0503289e19acc9848295f81aeacaca8ec0be
Barış Gündüz <baris.gunduz@devstup.org>
2021-03-04 12:23:50 +0300

    ssr -  generate static pages for SEO

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,8 +1,16 @@
 import React from "react";
 import Layout from "./Layout";
+import { Helmet } from "react-helmet";
 
 const Home = () => (
     <Layout>
+        <Helmet>
+            <title>MERN Stack</title>
+            <meta
+                name="description"
+                content="MERN Stack React Cloudinary Sendgrid SSR App"
+            />
+        </Helmet>
         <div className="container text-center">
             <h1 className="p-5">Home</h1>
             <hr />
